f65317891bb54c5d8fe3fa38888780bbf2fbeb3d
Jon Phillips <jon@rejon.org>
2017-04-26 05:48:58 +0000

    updates

---
+++ index.php
@@ -0,0 +1,3 @@
+<h2><blink>service.fabricatorz.com</blink></h2>
+<p>Code here: <a href="https://github.com/Fabricatorz/service.fabricatorz.com">https://github.com/Fabricatorz/service.fabricatorz.com</a></p>
+<p><a href="mailto:love@fabricatorz.com">love@fabricatorz.com</a></p>
